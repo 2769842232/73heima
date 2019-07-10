e73af61765a24c1fb8d1e17f511989543af45126
2769842232 <2769842232@qq.com>
2019-07-10 16:33:12 +0800

    完成卡片功能

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,13 +2,44 @@
     <div class="login-center">
       <el-card class="box">
         <img src="../../assets/image/logo_index.png" alt="">
+        <el-form>
+          <el-form-item>
+             <el-input v-model="form.name" placeholder="请输入手机号"></el-input>
+          </el-form-item>
+           <el-form-item>
+             <el-input  placeholder="请输入验证码" style="width:200px" ></el-input>
+             <el-button type="primary" style="margin-left:40px" @click="dian()">发送验证码</el-button>
+          </el-form-item>
+          <el-form-item>
+            <el-checkbox v-model="checked"></el-checkbox>
+              我已阅读并同意<el-link  :underline="false" type="primary">用户协议</el-link>和<el-link  :underline="false" type="primary">隐私条款</el-link>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary"  style="width:100%" @click="deng()">登录</el-button>
+          </el-form-item>
+        </el-form>
       </el-card>
     </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      form: {
+        name: ''
+      },
+      checked: true
+    }
+  },
+  methods: {
+    deng () {
+      alert('登录成功')
+    },
+    dian () {
+      alert('已发送')
+    }
+  }
 }
 </script>
 
@@ -29,7 +60,7 @@ export default {
       transform: translate(-50%,-50%);
       img{
         display: block;
-        margin: 0px auto;
+        margin: 0px auto 10px;
       }
     }
   }
