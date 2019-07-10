60e0e7794a6d80dace2b7b82560afc2d5e271bb9
2769842232 <2769842232@qq.com>
2019-07-10 16:01:09 +0800

    显示登录页面 一级路由完成

--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,5 +1,9 @@
 <template>
-    <div><h1>enen</h1></div>
+    <div class="login-center">
+      <el-card class="box">
+        <img src="../../assets/image/logo_index.png" alt="">
+      </el-card>
+    </div>
 </template>
 
 <script>
@@ -9,5 +13,24 @@ export default {
 </script>
 
 <style scoped lang="less">
-
+  .login-center{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top:0;
+    left: 0;
+    background: url(../../assets/image/login_bg.jpg) no-repeat center;
+    .box{
+      width: 400px;
+      height: 350px;
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      img{
+        display: block;
+        margin: 0px auto;
+      }
+    }
+  }
 </style>
