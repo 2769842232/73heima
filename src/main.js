318a968038bba2e58c7bce0ef923d6d75972d423
2769842232 <2769842232@qq.com>
2019-07-11 16:24:20 +0800

    完成小卡片的登录

--- src/main.js
+++ src/main.js
@@ -3,6 +3,10 @@ import App from './App.vue'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import router from '@/router'
+import axios from 'axios'
+// 注册
+Vue.prototype.axios = axios
+
 Vue.use(ElementUI)
 Vue.config.productionTip = false
 
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -2,20 +2,21 @@
     <div class="login-center">
       <el-card class="box">
         <img src="../../assets/image/logo_index.png" alt="">
-        <el-form>
-          <el-form-item>
-             <el-input v-model="form.name" placeholder="请输入手机号"></el-input>
+        <!-- 通过 'rules' 属性传入约定的验证规则 -->
+        <el-form ref="loginForm" status-icon :model="loginForm" :rules='loginRules'>
+          <el-form-item prop='mobile'>
+             <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
           </el-form-item>
-           <el-form-item>
-             <el-input  placeholder="请输入验证码" style="width:200px" ></el-input>
-             <el-button type="primary" style="margin-left:40px" @click="dian()">发送验证码</el-button>
-          </el-form-item>
-          <el-form-item>
-            <el-checkbox v-model="checked"></el-checkbox>
-              我已阅读并同意<el-link  :underline="false" type="primary">用户协议</el-link>和<el-link  :underline="false" type="primary">隐私条款</el-link>
+           <el-form-item prop='code'>
+             <el-input v-model="loginForm.code"   placeholder="请输入验证码" style="width:200px" ></el-input>
+             <el-button type="primary" style="float:right" >发送验证码</el-button>
           </el-form-item>
           <el-form-item>
-            <el-button type="primary"  style="width:100%" @click="deng()">登录</el-button>
+            <el-checkbox v-model="checked"></el-checkbox>我已阅读并同意
+             <el-link  :underline="false" type="primary">用户协议</el-link>和<el-link  :underline="false" type="primary">隐私条款</el-link>
+          </el-form-item>
+          <el-form-item>
+            <el-button type="primary"  style="width:100%" @click="login()" >登录</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -25,19 +26,56 @@
 <script>
 export default {
   data () {
+    // 检验手机号的
+    const checkMobile = (rule, value, callback) => {
+      if (/^1[3-9]\d{9}$/.test(value)) {
+        callback()
+      } else {
+        callback(new Error('手机号格式不对'))
+      }
+    }
     return {
-      form: {
-        name: ''
+      loginForm: {
+        mobile: '13911111111',
+        code: '246810'
+      },
+      loginRules: {
+        // 表单验证规则对象
+        mobile: [
+          // 验证规则 是否必填 长度 格式
+          { required: true, message: '手机号必填', tirgger: 'blur' },
+          { validator: checkMobile, trigger: 'blur' }
+        ],
+        code: [
+          { required: true, message: '验证码必填', tirgger: 'blur' },
+          { len: 6, message: '必须是6位', trigger: 'blur' }
+        ]
       },
       checked: true
     }
   },
   methods: {
-    deng () {
-      alert('登录成功')
-    },
-    dian () {
-      alert('已发送')
+    login () {
+      // 整体表单的验证
+      // 对整个表单进行校验的方法，参数为一个回调函数。
+      // 该回调函数会在校验结束后被调用，并传入两个参数
+      // ：是否校验成功和未通过校验的字段。若不传入回调函数，则会返回一个 promise
+      this.$refs.loginForm.validate(valid => {
+        if (valid) {
+          // 如果验证成功 进行登录
+          this.axios
+            .post('http://ttapi.research.itcast.cn/mp/v1_0/authorizations', this.loginForm)
+            .then((res) => {
+              const data = res.data
+              console.log(data)
+              // 登录成功之后跳转页面
+              // 保存登录状态
+              this.$router.push('/')
+            }).catch(() => {
+              this.$message.error('用户名或密码错误')
+            })
+        }
+      })
     }
   }
 }
