ea38bc24e8d4410a5d47988b5ce009ff4c7f0cf6
2769842232 <2769842232@qq.com>
2019-07-12 17:45:33 +0800

    全局的前置守卫

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '../views/login/login.vue'
 import Shou from '../views/Shou/shou.vue'
 import Huan from '../views/welcome/huan.vue'
+// 完成404页面
 import NotFound from '../views/404/index.vue'
 Vue.use(VueRouter)
 
@@ -18,7 +19,16 @@ const router = new VueRouter({
         { name: 'huan', path: '/Shou', component: Huan }
       ]
     },
+    // 路由规则的最后
+    // 路径走到这个位置 证明没有任何处理程序去处理这个路劲
     { path: '*', name: 'nofound', component: NotFound }
   ]
+})
+
+// 注册一个全局的前置导航守卫
+router.beforeEach((to, form, next) => {
+  const user = window.sessionStorage.getItem('tt')
+  if (to.path !== '/login' && !user) { return next('/login') }
+  next()
 })
 export default router
