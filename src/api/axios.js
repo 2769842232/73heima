4e145023264bee8fc19f17e551a122d3127781b0
2769842232 <2769842232@qq.com>
2019-07-12 19:10:55 +0800

    封装axios并配置

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,1 +1,10 @@
 // 封装axios
+// 默认配置项
+import axios from 'axios'
+const instance = axios.create({
+  baseURL: 'http://ttapi.research.itcast.cn/mp/v1_0/',
+  headers: {
+    Authorization: 'Bearer' + JSON.parse(window.sessionStorage.getItem('tt')).token
+  }
+})
+export default instance
