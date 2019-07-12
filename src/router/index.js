39ee484e87d2eae84699b011b64ee0f7006ece62
2769842232 <2769842232@qq.com>
2019-07-12 15:53:25 +0800

    加注释

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import VueRouter from 'vue-router'
 import Login from '../views/login/login.vue'
 import Shou from '../views/Shou/shou.vue'
 import Huan from '../views/welcome/huan.vue'
+// 完成404页面
 import NotFound from '../views/404/index.vue'
 Vue.use(VueRouter)
 
@@ -18,6 +19,8 @@ const router = new VueRouter({
         { name: 'huan', path: '/Shou', component: Huan }
       ]
     },
+    // 路由规则的最后
+    // 路径走到这个位置 证明没有任何处理程序去处理这个路劲
     { path: '*', name: 'nofound', component: NotFound }
   ]
 })
