e6cda8171628bae12962c3f45dfa9f6ae25eb6f5
2769842232 <2769842232@qq.com>
2019-07-12 15:45:36 +0800

    完成404显示

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,9 @@ import VueRouter from 'vue-router'
 import Login from '../views/login/login.vue'
 import Shou from '../views/Shou/shou.vue'
 import Huan from '../views/welcome/huan.vue'
+import NotFound from '../views/404/index.vue'
 Vue.use(VueRouter)
+
 const router = new VueRouter({
   routes: [
     { name: 'home', path: '/', redirect: '/login' },
@@ -15,7 +17,8 @@ const router = new VueRouter({
       children: [
         { name: 'huan', path: '/Shou', component: Huan }
       ]
-    }
+    },
+    { path: '*', name: 'nofound', component: NotFound }
   ]
 })
 export default router
---
+++ src/views/404/index.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="No-center">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped lang="less">
+  .No-center{
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background:url(../../assets/image/404.png) no-repeat center /cover;
+  }
+</style>
